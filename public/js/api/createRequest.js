a7f994c73f9453488387f82e5c13e605ee6459b3
Anton <antonch92@gmail.com>
2022-11-19 15:16:16 +0100

    attemption

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,7 +1,19 @@
+const { urlencoded, response } = require("express");
+
 /**
  * Основная функция для совершения запросов
  * на сервер.
  * */
 const createRequest = (options = {}) => {
-
+    if(options === 'GET'){
+        options.url = 'https://example.com';
+        options.data = {
+            mail: 'ivan@biz.pro',
+            password: 'odinodin',
+        };
+        options.responseType = 'json';
+        method = options;
+        console.log(response);
+    }
 };
+
