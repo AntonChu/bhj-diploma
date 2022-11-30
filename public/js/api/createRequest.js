2ca6d711836af9f51d6f5d07db634764344b2659
Anton <antonch92@gmail.com>
2022-11-30 20:53:33 +0100

    fourth commit

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,16 +1,27 @@
 const createRequest = (options = {}) => {
-  createRequest({
-    url: this.URL + '/login',
-    method: 'POST',
-    responseType: 'json',
-    data,
-    callback: (err, response) => {
-      if (response && response.user) {
-        this.setCurrent(response.user);
-        console.log('xzcxzbvnm,bmnbvccvbngmh,j,hmgnhfdsafghj');
-      }
-      console.log('jbdgbdfbdf');
-      callback(err, response);
+  const request = new XMLHttpRequest;
+  request.responseType = 'json'; 
+  request.url = options.url;
+  let formData = new FormData();
+  request.method = options.method;
+
+  if(request.method === 'GET'){
+    request.url += "?";
+    for (let item in options.data){
+      request.url += item + '=' + options.data[item] + '&';
     }
-  });
+    request.url.slice(0, -1);
+  }else{
+    for (let item in options.data){
+      formData.append(`${item}`, options.data[item]);
+    }
+  }
+
+  request.open(request.method, request.url);
+  request.send(formData);
+  request.onload = function(){
+    let err = null;
+    let response = xhr.response;
+    options.callback(err, response)
+  }
 };
